b56eecb9a826ceeedf5dd39eb271540272f44901
kecasper <kecasper@gmail.com>
2021-06-08 20:20:29 +0200

    192. Resolución de la tarea - Validaciones

---
+++ src/app/pages/template/template.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-template',
+  templateUrl: './template.component.html',
+  styleUrls: ['./template.component.css']
+})
+export class TemplateComponent implements OnInit {
+
+  usuario = {
+    nombre: 'Kevin',
+    apellido:'Castillo',
+    correo: 'kevin@gmail.com'
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  guardar( forma: NgForm ) {
+    console.log( forma );
+    console.log( forma.value );
+  }
+
+}
