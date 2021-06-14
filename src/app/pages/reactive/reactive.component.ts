1752d78febff2c5c8b81bee7099ada249bb9fe6e
kecasper <kecasper@gmail.com>
2021-06-14 19:18:46 +0200

    203. Reactivo: Validación visual de los campos anidados

--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'app-reactive',
   templateUrl: './reactive.component.html',
@@ -18,18 +18,56 @@ export class ReactiveComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  get nombreNoValido() {
+    return this.forma.get('nombre').invalid && this.forma.get('nombre').touched
+  }
+
+  get apellidoNoValido() {
+    return this.forma.get('apellido').invalid && this.forma.get('apellido').touched
+  }
+
+  get correoNoValido() {
+    return this.forma.get('correo').invalid && this.forma.get('correo').touched
+  }
+
+  get distritoNoValido() {
+    return this.forma.get('direccion.distrito').invalid && this.forma.get('direccion.distrito').touched
+  }
+
+  get ciudadNoValido() {
+    return this.forma.get('direccion.ciudad').invalid && this.forma.get('direccion.ciudad').touched
+  }
+
   crearFormulario() {
 
     this.forma = this.fb.group({
-      nombre : ['Kevin'],
-      apellido : ['Castillo'],
-      correo: ['abc@abc.com'],
+      nombre : ['', [ Validators.required, Validators.minLength(5) ] ],
+      apellido : ['', Validators.required ],
+      correo: ['', [ Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$') ] ],
+      direccion: this.fb.group({
+        distrito: ['', Validators.required ],
+        ciudad: ['', Validators.required ]
+      })
     });
 
   }
 
   guardar() {
     console.log( this.forma );
+
+    if( this.forma.invalid ) {
+
+      return Object.values( this.forma.controls ).forEach( control => {
+
+        if ( control instanceof FormGroup ) {
+          Object.values( control.controls ).forEach( control => control.markAsTouched() );
+        } else {
+          control.markAsTouched();
+        }
+      });
+
+    }
+
   }
 
 }
