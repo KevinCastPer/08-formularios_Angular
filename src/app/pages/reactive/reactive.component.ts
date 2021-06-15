6c2e391e4772ca16f7734ec4e41ece94305cf2d1
kecasper <kecasper@gmail.com>
2021-06-15 20:29:45 +0200

    finalizado del proyecto formulario template y reactive

--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
+import { ValidadoresService } from 'src/app/services/validadores.service';
 @Component({
   selector: 'app-reactive',
   templateUrl: './reactive.component.html',
@@ -9,13 +10,20 @@ export class ReactiveComponent implements OnInit {
 
   forma: FormGroup;
 
-  constructor( private fb: FormBuilder ) {
+  constructor( private fb: FormBuilder,
+              private validadores: ValidadoresService ) {
 
     this.crearFormulario();
+    this.cargarDataAlFormulario();
+    this.crearListeners();
 
   }
 
   ngOnInit(): void {
+  }
+
+  get pasatiempos() {
+    return this.forma.get('pasatiempos') as FormArray;
   }
 
   get nombreNoValido() {
@@ -30,6 +38,10 @@ export class ReactiveComponent implements OnInit {
     return this.forma.get('correo').invalid && this.forma.get('correo').touched
   }
 
+  get usuarioNoValido() {
+    return this.forma.get('usuario').invalid && this.forma.get('usuario').touched
+  }
+
   get distritoNoValido() {
     return this.forma.get('direccion.distrito').invalid && this.forma.get('direccion.distrito').touched
   }
@@ -38,18 +50,72 @@ export class ReactiveComponent implements OnInit {
     return this.forma.get('direccion.ciudad').invalid && this.forma.get('direccion.ciudad').touched
   }
 
+  get pass1NoValido() {
+    return this.forma.get('pass1').invalid && this.forma.get('pass1').touched
+  }
+
+  get pass2NoValido() {
+    const pass1 = this.forma.get('pass1').value;
+    const pass2 = this.forma.get('pass2').value;
+
+    return ( pass1 === pass2 ) ? false : true;
+  }
+
   crearFormulario() {
 
     this.forma = this.fb.group({
       nombre : ['', [ Validators.required, Validators.minLength(5) ] ],
-      apellido : ['', Validators.required ],
+      apellido : ['', [Validators.required, this.validadores.noKevin] ],
       correo: ['', [ Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$') ] ],
+      usuario: ['', , this.validadores.existeUsuario ],
+      pass1: ['', Validators.required ],
+      pass2: ['', Validators.required ],
       direccion: this.fb.group({
         distrito: ['', Validators.required ],
         ciudad: ['', Validators.required ]
-      })
+      }),
+      pasatiempos: this.fb.array([])
+    },{
+      Validators: this.validadores.passwordsIguales('pass1','pass2')
     });
 
+  }
+
+  crearListeners() {
+    // this.forma.valueChanges.subscribe( valor => {
+    //   console.log(valor);
+    // });
+
+    // this.forma.statusChanges.subscribe( status => console.log({status}));
+    this.forma.get('nombre').valueChanges.subscribe( console.log );
+  }
+
+
+  cargarDataAlFormulario() {
+
+    // this.forma.setValue({
+      this.forma.reset({
+      nombre: 'Kevin',
+      apellido: 'Castillo',
+      correo: 'kevin@correo.com',
+      pass1: '123',
+      pass2: '123',
+      direccion: {
+        distrito: 'Andalucia',
+        ciudad: 'Granada'
+      }
+    });
+
+    // ['Comer', 'Dormir'].forEach( valor => this.pasatiempos.push( this.fb.control(valor) ) );
+
+  }
+
+  agregarPasatiempo() {
+    this.pasatiempos.push( this.fb.control('') );
+  }
+
+  borrarPasatiempo(i: number ) {
+    this.pasatiempos.removeAt(i);
   }
 
   guardar() {
@@ -67,6 +133,11 @@ export class ReactiveComponent implements OnInit {
       });
 
     }
+
+    // Posteo de información
+    this.forma.reset({
+      nombre: 'Sin Nombre'
+    });
 
   }
 
